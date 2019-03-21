9f9a51df1db49224b54e874f6fd4c85268e224cf
Paul Weidner <paul.weidner@gmail.com>
2019-03-20 21:06:55 -0700

    Rename authenticated to success to match README usage

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,15 +4,15 @@ from .test_credentials import valid_credentials
 
 def test_invalid_login():
     farm = farmOS.farmOS('test.farmos.net', 'username', 'password')
-    authenticated = farm.authenticate()
+    success = farm.authenticate()
 
-    assert authenticated is False
+    assert success is False
 
 def test_valid_login():
     farm = farmOS.farmOS(**valid_credentials)
-    authenticated = farm.authenticate()
+    success = farm.authenticate()
 
-    assert authenticated is True
+    assert success is True
 
 def test_valid_record_request():
     """ Verifies that get_records pulls logs from the server """
