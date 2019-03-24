85dab0887ce88f0aeacb77153033d912b8cbee12
Paul Weidner <paul.weidner@gmail.com>
2019-03-23 22:00:27 -0700

    Add test for farm.info() method

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -19,6 +19,18 @@ def test_valid_login():
 def test_get_records_logs(test_farm):
     """ Test pulling logs from the server """
     logs = test_farm.logs.get()
+#
+# Test farm info method
+#
+
+def test_get_farm_info(test_farm):
+    info = test_farm.info()
+
+    assert 'name' in info
+    assert 'url' in info
+    assert 'api_version' in info
+    assert 'user' in info
+
 
     assert len(logs) > 0
 
