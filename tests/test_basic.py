976237f68c5d0b4122a8868ab6d6fde6d13d1569
Paul Weidner <paul.weidner@gmail.com>
2019-03-25 12:48:29 -0700

    Update farm_term to test .get() with vocab name and term id

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -116,13 +116,23 @@ def test_get_all_taxonomy_terms(test_farm):
 
     assert len(terms) > 0
 
-def test_get_farm_terms_filtered_by_single_vocabulary(test_farm):
+def test_get_farm_terms_filtered_by_single_vocabulary_name(test_farm):
     vocabulary_name = 'farm_crops'
 
     terms = test_farm.term.get(vocabulary_name)
 
     assert len(terms) > 0
-    assert terms[0]['vocabulary'] == vocabulary_name
+    # Assert all terms retrieved are from the same vocabulary
+    # (cannot check vocabulary name in response)
+    assert terms[0]['vocabulary']['id'] == terms[1]['vocabulary']['id']
+
+def test_get_farm_terms_filtered_by_single_vocabulary_tid(test_farm):
+    vocabulary_tid = 7
+
+    term = test_farm.term.get(vocabulary_tid)
+
+    assert 'vocabulary' in term
+    assert int(term['tid']) == vocabulary_tid
 
 def test_get_farm_term_filtered_by_multiple_vocabulary(test_farm):
     vocabulary_name = 'farm_crops'
