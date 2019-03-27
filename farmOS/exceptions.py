11dcec91e72e2a7c8a24968bb7c8f4035d226309
Paul Weidner <paul.weidner@gmail.com>
2019-03-26 20:34:45 -0700

    Add exceptions file, create base class and NotAuthenticatedError

---
+++ farmOS/exceptions.py
@@ -0,0 +1,21 @@
+class FarmosAPIError(Exception):
+    data = {}
+    code = -1
+    message = "An unknown error occurred"
+
+    def __init__(self, message=None, code=None, data={}, response=None):
+        if message:
+            self.message = message
+        if code:
+            self.code = code
+        if data:
+            self.data = data
+
+    def __str__(self):
+        if self.code:
+            return '{}: {}'.format(self.code, self.message)
+        return self.message
+
+class NotAuthenticatedError(FarmosAPIError):
+    message = "APISession not authenticated before the request was made. Call farm.authentiate() before making requests"
+    pass
