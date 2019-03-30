b8e5b1c13bc9c18907b25615e9ad55bf31133cb7
Paul Weidner <paul.weidner@gmail.com>
2019-03-30 12:13:45 -0700

    Add area.send() and area.delete()

--- tests/functional/test_area.py
+++ tests/functional/test_area.py
@@ -1,3 +1,15 @@
+def _search_for_vocab_id(name, vocabs):
+    return [vocab for vocab in vocabs if vocab['machine_name'] == name]
+
+test_area = {
+    'name':'Testing area',
+    'area_type':'field',
+    'vocabulary': {
+        'id':None,
+        'resource':'taxonomy_vocabulary'
+    }
+}
+
 #
 # Test farm area methods
 #
@@ -24,3 +36,21 @@ def test_get_farm_areas_by_id(test_farm):
 
     assert 'tid' in area
     assert int(area['tid']) == area_tid
+
+def test_create_area(test_farm):
+    # Find the vocab ID for farm_areas
+    vocabs = test_farm.term.vocabularies()
+    farm_areas_id = _search_for_vocab_id('farm_areas', vocabs)[0]['vid']
+    # Update the test_area with the vid
+    test_area['vocabulary']['id'] = farm_areas_id
+
+    response = test_farm.area.send(test_area)
+    print(test_area, response)
+    assert 'id' in response
+
+    # Once created, add 'id' to test_asset
+    test_area['id'] = response['id']
+
+def test_delete_area(test_farm):
+    response = test_farm.area.delete(int(test_area['id']))
+    assert response.status_code == 200
