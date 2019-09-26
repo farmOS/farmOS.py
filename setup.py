b4b07f71f1d15be1576f465d3b3775f2ee65b1fd
Paul Weidner <paul.weidner@gmail.com>
2019-09-25 20:32:59 -0700

    Release v0.1.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='farmOS',
-    version='0.0.2',
+    version='0.1.0',
     author='farmOS team',
     author_email='mike@mstenta.net',
     description='A Python library for interacting with farmOS over API. ',
