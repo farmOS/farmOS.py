7273baa306627f0b97f7b83b5bd1091b058dff11
paul121 <paul.weidner@gmail.com>
2020-08-05 11:50:41 -0700

    Add additional test for OAuth. #33

--- tests/functional/test_auth.py
+++ tests/functional/test_auth.py
@@ -1,31 +1,53 @@
 import pytest
 import os
 
-from farmOS import farmOS, HTTPError
-from oauthlib.oauth2 import InvalidGrantError
+from farmOS import farmOS, HTTPError, InvalidGrantError, InvalidClientError, InvalidScopeError
 
 from tests.conftest import farmOS_testing_server
 
+# Variables for testing.
+FARMOS_HOSTNAME = os.getenv("FARMOS_HOSTNAME")
+FARMOS_OAUTH_USERNAME = os.getenv("FARMOS_OAUTH_USERNAME")
+FARMOS_OAUTH_PASSWORD = os.getenv("FARMOS_OAUTH_PASSWORD")
 
-# Test authentication
+
 @farmOS_testing_server
 def test_invalid_login():
     with pytest.raises(InvalidGrantError):
-        farm = farmOS('test.farmos.net')
+        farm = farmOS(FARMOS_HOSTNAME)
         farm.authorize('username', 'password')
+
+
+@farmOS_testing_server
+def test_invalid_client_id():
+    with pytest.raises(InvalidClientError):
+        farm = farmOS(FARMOS_HOSTNAME, client_id='bad_client')
+        farm.authorize(FARMOS_OAUTH_USERNAME, FARMOS_OAUTH_PASSWORD)
+
+
+@farmOS_testing_server
+def test_invalid_client_secret():
+    with pytest.raises(InvalidClientError):
+        farm = farmOS(FARMOS_HOSTNAME, client_id='farm', client_secret='bad_pass')
+        farm.authorize(FARMOS_OAUTH_USERNAME, FARMOS_OAUTH_PASSWORD)
+
+
+@farmOS_testing_server
+def test_invalid_scopet():
+    with pytest.raises(InvalidScopeError):
+        farm = farmOS(FARMOS_HOSTNAME, scope="bad_scope")
+        farm.authorize(FARMOS_OAUTH_USERNAME, FARMOS_OAUTH_PASSWORD, scope="bad_scope")
 
 
 @farmOS_testing_server
 def test_unauthorized_request(test_farm):
     with pytest.raises(HTTPError, match=r"403 *."):
-        farm = farmOS('test.farmos.net')
+        farm = farmOS(FARMOS_HOSTNAME)
         farm.log.get()
 
 
 @farmOS_testing_server
 def test_valid_login(test_farm):
-    FARMOS_OAUTH_USERNAME = os.getenv("FARMOS_OAUTH_USERNAME")
-    FARMOS_OAUTH_PASSWORD = os.getenv("FARMOS_OAUTH_PASSWORD")
     token = test_farm.authorize(username=FARMOS_OAUTH_USERNAME, password=FARMOS_OAUTH_PASSWORD)
 
     assert 'access_token' in token
