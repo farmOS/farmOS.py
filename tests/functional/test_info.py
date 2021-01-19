84764e3c2d2ad0c7e2efef345c65b07188ef2027
paul121 <paul.weidner@gmail.com>
2021-01-19 13:33:35 -0800

    Refactor info test for a 2.x server.

--- tests/functional/test_info.py
+++ tests/functional/test_info.py
@@ -7,8 +7,12 @@ from tests.conftest import farmOS_testing_server
 def test_get_farm_info(test_farm):
     info = test_farm.info()
 
-    assert "name" in info
-    assert "url" in info
-    assert "api_version" in info
-    assert "user" in info
-    assert "resources" in info
+    assert "links" in info
+    assert "meta" in info
+
+    # Test farm info.
+    assert "farm" in info["meta"]
+    farm_info = info["meta"]["farm"]
+    assert "name" in farm_info
+    assert "url" in farm_info
+    assert "version" in farm_info
